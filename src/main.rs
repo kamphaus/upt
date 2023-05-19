c25d2e82053a6e0806197585ac7fa9567abe236f
Christophe Kamphaus <christophe.kamphaus@jemmic.com>
2023-05-19 09:41:21 +0200

    Correctly render the duration

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
-use clap::{CommandFactory, Parser, Subcommand, Command};
+use clap::{CommandFactory, Parser, Subcommand, Command, crate_version};
 use clap_complete::{generate, Generator, Shell};
 use std::io;
+use std::{thread, time};
+use std::ops::Sub;
+use chrono::{DateTime, Duration, Local, Utc};
+use chrono;
+use chrono_humanize::{Accuracy, Tense};
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[clap(author="Christophe Kamphaus", about="A simple uptime CLI tool")]
+#[command(author, about, long_about = None)]
 struct Cli {
-    /// Optional name to operate on
-    name: Option<String>,
+    /// Reset the uptime to now
+    #[arg(short, long)]
+    reset: bool,
 
-    /// Turn debugging information on
-    //#[arg(short, long, action = clap::ArgAction::Count)]
-    //debug: u8,
+    /// Watch the uptime
+    #[arg(short, long)]
+    watch: bool,
+
+    /// Print start date time
+    #[arg(short, long)]
+    start: bool,
+
+    /// Instead of human readable duration print in ISO 8601 format
+    #[arg(short, long)]
+    iso: bool,
+
+    /// Print version
+    #[arg(short, long)]
+    version: bool, // we explicitly use version flag, instead of the clap version macro so we can have lowercase 'v' short flag
 
     #[command(subcommand)]
     command: Option<Commands>,
@@ -18,27 +37,90 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Commands {
-    /// does testing things
+    /// Generate autocompletion scripts for upt for the specified shell.
     Completion {
-        shell: Option<Shell>
+        shell: Shell
     },
 }
 
+/// Prints the given autocompletion for the passed shell and command to stdout
 fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
     generate(gen, cmd, cmd.get_name().to_string(), &mut io::stdout());
 }
 
+/// print the given start time, either in strict iso format or in simplified iso format
+fn print_start(start: DateTime<Local>, strict_iso: bool) {
+    if strict_iso {
+        println!("Started {:?}", start);
+    } else {
+        println!("Started {}", start.format("%Y-%m-%d %H:%M:%S").to_string());
+    }
+}
+
+fn render_duration(start: DateTime<Utc>, iso: bool) -> String {
+    let duration = Utc::now().sub(start);
+    if iso {
+        return duration.to_string()
+    }
+    let truncated = Duration::seconds(duration.num_seconds());
+    let formatted = chrono_humanize::HumanTime::from(truncated).to_text_en(Accuracy::Precise, Tense::Present);
+    return formatted.replace(" and", ",");
+}
+
 fn main() {
     let cli = Cli::parse();
-    let mut app = Cli::command();
     match &cli.command {
         Some(Commands::Completion { shell}) => {
-            let s = shell.unwrap_or(Shell::Bash);
-            print_completions(s, &mut app);
-            println!("Not printing testing lists...");
+            let mut app = Cli::command();
+            print_completions(*shell, &mut app);
+            return
         }
         None => {
-            println!("Hello, world!");
+            // handle below
         }
     }
+    if cli.version {
+        let cli = Cli::command();
+        println!("{} {}", cli.get_display_name().unwrap_or_else(|| cli.get_name()), crate_version!()); // same implementation as the default clap version command
+        return;
+    }
+    let start_result = uptime_lib::get();
+    if start_result.is_err() {
+        eprintln!("Cannot get uptime of system {}", start_result.unwrap_err());
+        return;
+    }
+    let now = Local::now();
+    // We assume that the system uptime is not impacted by any timezone changes.
+    // To ensure that any timezone changes do not impact the consistency between start datetime and duration
+    // we always represent it in UTC and just print the start time in the local timezone.
+    let mut start = now.checked_sub_signed(Duration::from_std(start_result.unwrap()).unwrap()).unwrap().with_timezone(&Utc);
+    // TODO: implement persistence
+    if cli.reset {
+        // reset the counter
+        start = now.with_timezone(&Utc);  // TODO: implement persistence
+    }
+    if cli.start {
+        print_start(start.with_timezone(&Local), cli.iso);
+    }
+    if cli.watch {
+        let sleep_millis = time::Duration::from_millis(5);
+        // TODO: hide the cursor
+        loop {
+            // print the duration
+            let duration = render_duration(start, cli.iso);
+            print!("{}", duration);
+            thread::sleep(sleep_millis);
+            for _i in 1..duration.len() {
+                print!("{}", '\r')
+            }
+        }
+    } else {
+        println!("{}", render_duration(start, cli.iso))
+    }
+}
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    Cli::command().debug_assert()
 }
