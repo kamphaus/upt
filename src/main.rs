98f8c2726c02ea1d28858ed8295a4a41fdf61ac9
Christophe Kamphaus <christophe.kamphaus@jemmic.com>
2023-06-04 14:48:38 +0200

    Cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -233,7 +233,16 @@ fn verify_cli() {
 fn test_date_parsing() {
     let nt = NaiveDateTime::from_timestamp_opt(1685871491, 0);
     let dt: DateTime<Utc> = DateTime::from_utc(nt.unwrap(), Utc);
-    assert_eq!(parse_time("2023-06-04T09:38:11.000000000+00:00".to_string()).unwrap(), dt);
-    assert_eq!(parse_time("2023-06-04T09:38:11.000000000+00:00\n".to_string()).unwrap(), dt);
-    assert_eq!(parse_time("2030604T09:xy:11.000000000+00:00".to_string()).is_err(), true)
+    assert_eq!(
+        parse_time("2023-06-04T09:38:11.000000000+00:00".to_string()).unwrap(),
+        dt
+    );
+    assert_eq!(
+        parse_time("2023-06-04T09:38:11.000000000+00:00\n".to_string()).unwrap(),
+        dt
+    );
+    assert_eq!(
+        parse_time("2030604T09:xy:11.000000000+00:00".to_string()).is_err(),
+        true
+    )
 }
